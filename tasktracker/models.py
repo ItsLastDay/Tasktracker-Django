9f1110e37527f5118bf7424d614f467aa83e36e5
Mikhail Koltsov <misha@koltsov.su>
2014-04-17 17:03:52 +0400

    wrote tag, task

--- tasktracker/models.py
+++ tasktracker/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Tag(models.Model):
-    pass
+    title = models.CharField(max_length=50, unique=True)
+    tasks = models.ManyToManyField(Task)
+
+    def __unicode__(self):
+        return self.title
 
 class Task(models.Model):
-    pass
+    title = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User)
+    rating = models.SmallIntegerField()
+    created_on = models.DateTimeField()
+    assigned_to = models.ManyToManyField(User)
+    expiration_date = models.DateTimeField()
+    description = models.TextField()
+    status = models.CharField(choices=[('cp', 'Closed'), ('op', 'Open')])
+
+    def __unicode__(self):
+        return self.title
 
 class User(models.Model):
-    pass
-
+    login = models.CharField(unique=True, editable=False)
+    
+    def __unicode__(self):
+        return self.login
 

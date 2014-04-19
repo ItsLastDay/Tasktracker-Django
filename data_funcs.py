a4cd81c2b3a3fa4cbe41ca9905133e958e7db7a1
Mikhail Koltsov <misha@koltsov.su>
2014-04-19 19:18:36 +0400

    wrote 7 queries

---
+++ data_funcs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello.settings")
+    from tasktracker.models import *
+
+    print '# 1: all tasks with status = open'
+    print Task.objects.open().values('pk', 'status')
+    print '\n\n'
+    print '# 2: all tasks with status = closed'
+    print Task.objects.closed().values('pk', 'status')
+    print '\n\n'
+    print '# 3: all tasks with no expiration date'
+    print Task.objects.infinite().values('pk', 'expiration_date')
+    print '\n\n'
+    print '# 4: all tasks with rating > 4 and assigned to > 2 users'
+    print Task.objects.query1().values('num_assigned', 'rating', 'pk')
+    print '\n\n'
+    print '# 5: all tasks with more than 3 tags' 
+    print Task.objects.query_tags().values('pk', 'num_tags')
+    print '\n\n'
+    print '# 6: popular tasks, top 10 from 60th place'
+    print Task.objects.get_popular(start=60, top=10).values('pk', 'rating')
+    print '\n\n'
+    print '# 7: tasks with valid date (expiration >= creation) or with no expiration date'
+    print Task.objects.valid().values('pk', 'created_on', 'expiration_date')
--- tasktracker/models.py
+++ tasktracker/models.py
@@ -16,9 +16,23 @@ class TaskManager(models.Manager):
         # 3: all tasks with no expiration date
         return self.filter(expiration_date__isnull=True)
 
-    def query1(self):
+    def query1(self, r_thresh=4, assigned_thresh=2):
         # 4: all tasks with rating > 4 and assigned to > 2 users
-        return self.filter(rating__gt=4).annotate(num_assigned=models.Count('assigned_to')).filter(num_assigned__gt=2)
+        return self.filter(rating__gt=r_thresh).annotate(num_assigned=models.Count('assigned_to')).filter(num_assigned__gt=assigned_thresh)
+
+    def query_tags(self, tags_thresh=3):
+        # 5: all tasks with more than 3 tags
+        return self.annotate(num_tags=models.Count('tag')).filter(num_tags__gt=tags_thresh)
+
+    def get_popular(self, start=0, top=10):
+        # 6: popular tasks, with offset
+        return self.order_by('rating').reverse()[start:start+top]
+
+    def valid(self):
+        # 7: tasks with valid date (expiration >= creation) or with no expiration date
+        q = models.Q(created_on__lt=models.F('expiration_date'))
+        q |= models.Q(expiration_date__isnull=True)
+        return self.filter(q)
 
 class Tag(models.Model):
     title = models.CharField(max_length=50, unique=True)
@@ -34,7 +48,7 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     created_by = models.ForeignKey('User', related_name='created_tasks')
     rating = models.IntegerField(db_index=True)
-    date_fmt = 'Please use the following format: YYYY-MM-DD HH:MM' # I don't know actual one
+    date_fmt = 'Please use the following format: YYYY-MM-DD HH:MM' 
     created_on = models.DateTimeField(help_text=date_fmt, db_index=True)
     assigned_to = models.ManyToManyField('User', related_name='assigned_tasks')
     expiration_date = models.DateTimeField(help_text=date_fmt, db_index=True, null=True, blank=True)
@@ -49,7 +63,7 @@ class Task(models.Model):
         pass
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ['pk', '-created_on']
 
 class User(models.Model):
     login = models.CharField(max_length=20, unique=True, editable=False)
